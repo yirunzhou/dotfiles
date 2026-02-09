e574e18dc6826c82f220d4a12c9dd5158085ea49
yirunzhou <zhouyirun1996@gmail.com>
2026-02-08 21:16:00 -0800

    init

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Create the config directory if it doesn't exist
+mkdir -p ~/.config
+
+# Symlink Neovim
+# ln -s [TARGET] [LINK_NAME]
+ln -sfn ~/dotfiles/nvim ~/.config/nvim-lazy
+
+# Symlink Tmux
+ln -sfn ~/dotfiles/tmux ~/.config/tmux
+
+# Symlink Zsh
+ln -sfn ~/dotfiles/zsh/.zshrc ~/.config/shell_profiles/.zshrc
+ln -sfn ~/dotfiles/zsh/.zshenv ~/.zshenv
